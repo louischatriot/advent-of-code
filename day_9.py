12082ad6c71b1b0e9fa319e20e9b3ceab8dfe5d0
Louis Chatriot <louis.chatriot@gmail.com>
2022-12-09 16:50:30 +0100

    Day 9

--- day_9.py
+++ day_9.py
@@ -3,14 +3,28 @@ with open("inputs/day_9.data") as file:
 
 movements = [(line[0], int(line[2:])) for line in lines]
 
+def new_pos(t, h):
+    tx, ty = t
+    hx, hy = h
 
-# Part 1
+    if tx == hx:
+        if ty + 1 < hy:
+            ty += 1
+        elif ty - 1 > hy:
+            ty -= 1
+    elif ty == hy:
+        if tx + 1 < hx:
+            tx += 1
+        elif tx - 1 > hx:
+            tx -= 1
+    else:
+        if abs(tx - hx) + abs(ty - hy) >= 3:
+            tx += 1 if tx < hx else -1
+            ty += 1 if ty < hy else -1
 
-tx, ty, hx, hy = 0, 0, 0, 0
-visited = set()
-visited.add((tx, ty))
+    return (tx, ty)
 
-for d, n in movements:
+def deltas(d):
     dx, dy = 0, 0
 
     if d == 'R':
@@ -24,29 +38,55 @@ for d, n in movements:
     else:
         raise ValueError("Unknown direction")
 
+    return (dx, dy)
+
+
+
+# Part 1
+
+tx, ty, hx, hy = 0, 0, 0, 0
+visited = set()
+visited.add((tx, ty))
+
+for d, n in movements:
+    dx, dy = deltas(d)
+
     for _ in range(0, n):
         hx += dx
         hy += dy
 
-        if tx == hx:
-            if ty + 1 < hy:
-                ty += 1
-            elif ty - 1 > hy:
-                ty -= 1
-        elif ty == hy:
-            if tx + 1 < hx:
-                tx += 1
-            elif tx - 1 > hx:
-                tx -= 1
-        else:
-            if abs(tx - hx) + abs(ty - hy) >= 3:
-                tx += 1 if tx < hx else -1
-                ty += 1 if ty < hy else -1
+        tx, ty = new_pos((tx, ty), (hx, hy))
 
         visited.add((tx, ty))
 
+print(len(visited))
+
+
+# Part 2
+
+rope = [(0, 0)] * 10
+visited = set()
+visited.add(rope[-1])
+
+for d, n in movements:
+    dx, dy = deltas(d)
+
+    for _ in range(0, n):
+        rope[0] = (rope[0][0] + dx, rope[0][1] + dy)
+
+        for i in range(1, len(rope)):
+            rope[i] = new_pos(rope[i], rope[i-1])
+
+
+        visited.add(rope[-1])
 
 print(len(visited))
+
+
+
+
+
+
 
 
 
