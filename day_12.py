d76c5889405ba902cd5a270873dfc0dad4bc9d3f
Louis Chatriot <louis.chatriot@gmail.com>
2022-12-12 14:54:30 +0100

    Day 12 part 2

--- day_12.py
+++ day_12.py
@@ -28,20 +28,40 @@ for i in range(0, N):
         transitions[i][j] = possible_dests
 
 
+def find_shortest_path_length(start, end):
+    paths = [[None for j in range(0, M)] for i in range(0, N)]
+    paths[start[0]][start[1]] = [start]
+    boundary = [start]
+
+    while paths[end[0]][end[1]] is None and len(boundary) > 0:
+        i0, j0 = boundary.pop(0)
+
+        for i, j in transitions[i0][j0]:
+            if paths[i][j] is None:
+                paths[i][j] = paths[i0][j0] + [(i, j)]
+                boundary.append((i, j))
+
+    if paths[end[0]][end[1]] is None:
+        return 999999
+    else:
+        return len(paths[end[0]][end[1]]) - 1
+
 # Part 1
-paths = [[None for j in range(0, M)] for i in range(0, N)]
-paths[start[0]][start[1]] = [start]
-boundary = [start]
-
-while paths[end[0]][end[1]] is None:
-    i0, j0 = boundary.pop(0)
-
-    for i, j in transitions[i0][j0]:
-        if paths[i][j] is None:
-            paths[i][j] = paths[i0][j0] + [(i, j)]
-            boundary.append((i, j))
+print(find_shortest_path_length(start, end))
 
 
-print(len(paths[end[0]][end[1]]))
+# Part 2
+# Brute forcing it is pretty inefficient we could do a search from the end but oh well it works for this input
+starts = []
+for i in range(0, N):
+    for j in range(0, M):
+        if map[i][j] == 'a':
+            starts.append((i, j))
+lengths = [find_shortest_path_length(s, end) for s in starts]
+print(min(lengths))
+
+
+
+
 
 
