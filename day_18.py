1099506cd4ae7b3ea08c3ea405a22121b02c2c63
Louis Chatriot <louis.chatriot@gmail.com>
2022-12-18 18:14:15 +0100

    Day 18

--- day_18.py
+++ day_18.py
@@ -1,10 +1,14 @@
 from time import time
 
-with open("inputs/day_18_example.data") as file:
+with open("inputs/day_18.data") as file:
     lines = [line.rstrip() for line in file]
 
 
 cubes = [tuple([int(c) for c in l.split(',')]) for l in lines]
+
+cube_check = set()
+for c in cubes:
+    cube_check.add(c)
 
 
 
@@ -31,7 +35,42 @@ print(res)
 
 # Part 2
 
+xs = [c[0] for c in cubes]
+xmin, xmax = min(xs) - 1, max(xs) + 1
+
+ys = [c[1] for c in cubes]
+ymin, ymax = min(ys) - 1, max(ys) + 1
+
+zs = [c[2] for c in cubes]
+zmin, zmax = min(zs) - 1, max(zs) + 1
+
+res = 0
+
+fringe = [(xmin, ymin, zmin)]
+checked = set()
 
 
+while len(fringe) > 0:
+    cube = fringe.pop(0)
+
+    if cube in checked:
+        continue
+
+    x, y, z = cube
+    checked.add(cube)
+
+    for cc in [(x+1, y, z), (x-1, y, z), (x, y+1, z), (x, y-1, z), (x, y, z+1), (x, y, z-1)]:
+        if not (xmin <= cc[0] <= xmax and ymin <= cc[1] <= ymax and zmin <= cc[2] <= zmax):
+            continue
+
+        if cc in cube_check:
+            res += 1
+        else:
+            if cc not in checked:
+                fringe.append(cc)
+
+
+
+print(res)
 
 
