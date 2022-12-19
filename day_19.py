df8d2f75839d8cd24f9be635812a677d2d817e04
Louis Chatriot <louis.chatriot@gmail.com>
2022-12-19 22:33:15 +0100

    Stash

--- day_19.py
+++ day_19.py
@@ -32,6 +32,120 @@ for line in lines:
     blueprints[blueprint_n] = robots_costs
 
 
-print(blueprints)
+def can_build(blueprint, resources, robot):
+    for r in blueprint[robot]:
+        if resources.get(r, 0) < blueprint[robot][r]:
+            return False
+    return True
+
+
+def time_until_next_robot(blueprint, resources, robots):
+    _resources = {k:v for k,v in resources.items()}
+    for res in range(0, blueprint['ore']['ore'] + 1):   # We know we have at least one ore robot so we have an upper bound to the time to next robot
+        if any(can_build(blueprint, _resources, robot) for robot in blueprint):
+            return res
+
+        for r, amt in robots.items():
+            _resources[r] += amt
+
+
+
+def time_until_can_build(blueprint, resources, robots, robot):
+    _resources = {k:v for k,v in resources.items()}
+    res = 0
+
+
+
+
+
+
+# Part 1
+def dfs(blueprint, resources, robots, remaining_time):
+    # print("====================================", remaining_time, resources, robots)
+
+
+    if remaining_time == 0:
+        return resources, robots
+
+    # if resources['geode'] >= 7:
+        # print(remaining_time, resources['geode'])
+
+
+    # That should not work, actually, as we can construct more geode robots
+    if remaining_time + resources['geode'] <= 11:
+        # if resources['geode'] >= 1:
+            # print(remaining_time, resources['geode'])
+
+        return resources, robots
+
+    t = time_until_next_robot(blueprint, resources, robots)
+    t = min(t, remaining_time)
+
+    # Produce during these t units of time
+    _resources = {k:v for k,v in resources.items()}
+    for r, amt in robots.items():
+        _resources[r] += amt * t
+    resources = _resources
+
+    if remaining_time - t == 0:
+        return resources, robots
+
+
+    # Not building anything
+    _resources = {k:v for k,v in resources.items()}
+    # _robots = {k:v for k,v in robots.items()}
+    for r, amt in robots.items():
+        _resources[r] += amt
+
+    __res, __rob = dfs(blueprint, _resources, robots, remaining_time - t - 1)
+
+
+    score = __res['geode']
+    resources_optim = __res
+    robots_optim = __rob
+
+
+    # Building one robot (not handling case where multiple robots are built at once, not sure it's possible from the docs)
+    for robot in blueprint:
+        if can_build(blueprint, resources, robot):
+            _resources = {k:v for k,v in resources.items()}
+            _robots = {k:v for k,v in robots.items()}
+
+            for r in blueprint[robot]:
+                _resources[r] -= blueprint[robot][r]
+
+            _robots[robot] += 1
+
+            for r, amt in _robots.items():
+                _resources[r] += amt
+
+            __res, __rob = dfs(blueprint, _resources, _robots, remaining_time - t - 1)
+            if __res['geode'] > score:
+                resources_optim = __res
+                robots_optim = __rob
+
+    return resources_optim, robots_optim
+
+
+
+bp_test = {
+    'ore': {'ore': 2},
+    'clay': {'ore': 2},
+    'obsidian': {'clay': 2, 'ore': 1},
+    'geode': {'obsidian': 1},
+}
+
+bp_test = blueprints[1]
+
+
+start = time()
+
+res, rob = dfs(bp_test, {r:0 for r in blueprints[1]}, {r:0 if r != 'ore' else 1 for r in blueprints[1]}, 24)
+
+print("================================")
+print(res)
+print(rob)
+
+print("==> Duration", time() - start)
 
 
