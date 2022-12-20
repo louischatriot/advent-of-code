ff961dc89c6bc147d05b839c27bd8a07a04e24cc
Louis Chatriot <louis.chatriot@gmail.com>
2022-12-20 23:14:35 +0100

    Day 20 part 1

--- day_20.py
+++ day_20.py
@@ -1,6 +1,6 @@
 from time import time
 
-with open("inputs/day_20_example.data") as file:
+with open("inputs/day_20.data") as file:
     lines = [line.rstrip() for line in file]
 
 data = [int(n) for n in lines]
@@ -17,58 +17,56 @@ def get_list(data, positions):
 
     return res
 
-print(data)
-print(positions)
-
-print("===========================")
-print("===========================")
-
 
 for i0, v in enumerate(data):
-    print("===============================", i0, v)
+    log = (0 <= i0 <= 5) or True
+
     _from = positions[i0]
     _to = _from + v
 
-    # print("from ", _from, " to ", _to)
+    while _to < 0:
+        _to = _to + N - 1
 
+    while _to >= N:
+        _to = _to - N + 1
 
-    if v == 0:
-        pass
-
-    elif v > 0:
-        if _to < N:
+    if v != 0:
+        if _from < _to:
             for i in range(0, N):
                 if _from < positions[i] <= _to:
                     positions[i] -= 1
 
         else:
-            _to = _to - N + 1
             for i in range(0, N):
                 if _to <= positions[i] < _from:
                     positions[i] += 1
-
-
-    elif v < 0:
-        if _to > 0:
-            for i in range(0, N):
-                if _to <= positions[i] < _from:
-                    positions[i] += 1
-
-        else:
-            _to = _to + N - 1
-            print("%%%%")
-            print(_to)
-            for i in range(0, N):
-                if _from < positions[i] <= _to:
-                    positions[i] -= 1
 
     positions[i0] = _to
-    print("POS ", positions)
 
 
 
-    l = get_list(data, positions)
-    print(l)
+l = get_list(data, positions)
+
+i0 = l.index(0)
+
+res = l[(i0 + 1000) % N] + l[(i0 + 2000) % N] + l[(i0 + 3000) % N]
+
+print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
