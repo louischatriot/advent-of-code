db067d9af46f39526ee11b7003ae46bedefe05e5
Louis Chatriot <louis.chatriot@gmail.com>
2023-11-23 08:50:06 +0100

    Ready to start 2021

---
+++ 2021/u.py
@@ -0,0 +1,54 @@
+def split_at_char(s, n):
+    l = s[0:n]
+    r = s[n:]
+    return (l, r)
+
+
+def get_pos(arr, i, j, di, dj):
+    if not (0 <= i + di < len(arr)):
+        return '.'
+
+    if not (0 <= j + dj < len(arr[i])):
+        return '.'
+
+    else:
+        return arr[i+di][j+dj]
+
+
+def get_visible(arr, i, j, di, dj):
+    while (0 <= i + di < len(arr)) and (0 <= j + dj < len(arr[i])):
+        i += di
+        j += dj
+        if arr[i][j] != '.':
+            return arr[i][j]
+
+    return '.'
+
+
+# Assuming same size
+def compare_2d_arrays(a1, a2):
+    for i in range(0, len(a1)):
+        for j in range(0, len(a1[0])):
+            if a1[i][j] != a2[i][j]:
+                return False
+
+    return True
+
+
+def __aps(res, rem):
+    if len(rem) == 0:
+        return res
+
+    r, rem = rem[0], rem[1:]
+
+    newres = []
+    for v in res:
+        newres.append(v)
+        newres.append(v + r)
+
+    return __aps(newres, rem)
+
+
+def all_partial_sums(l):
+    return __aps([0], l)
+
