99978f1c47b9600e9a31474464e02e3b925944ef
Louis Chatriot <louis.chatriot@gmail.com>
2024-01-26 12:51:38 +0100

    Day 20 2019 done now to the intcodes

--- 2019/day_20.py
+++ 2019/day_20.py
@@ -14,29 +14,6 @@ with open(fn) as file:
 
 
 # PART 1
-portals = dict()
-portal_names = set()
-
-def add_portal(portals, name, i, j):
-    key = f"{name}-1"
-    if key in portals:
-        key = f"{name}-2"
-
-    portals[key] = (i, j)
-
-    if name not in ['AA', 'ZZ']:
-        portal_names.add(name)
-
-left = re.compile('[A-Z]{2}\.')
-right = re.compile('\.[A-Z]{2}')
-
-for i, l in enumerate(lines):
-    for m in left.finditer(l):
-        add_portal(portals, m.group()[0:2], i, m.start() + 2)
-
-    for m in right.finditer(l):
-        add_portal(portals, m.group()[1:], i, m.start())
-
 def get_matrix_from_lines(lines):
     matrix = [[c for c in l] for l in lines]
     # Padding
@@ -46,6 +23,38 @@ def get_matrix_from_lines(lines):
             l.append(' ')
     return matrix
 
+portals = dict()
+portal_names = set()
+
+def add_portal(portals, name, i, j, I, J):
+    if i in [2, I-3] or j in [2, J-3]:
+        key = f"{name}-1"
+    else:
+        key = f"{name}-2"
+
+    if key in portals:
+        raise ValueError("Can't have the same key twice")
+
+    portals[key] = (i, j)
+
+    if name not in ['AA', 'ZZ']:
+        portal_names.add(name)
+
+left = re.compile('[A-Z]{2}\.')
+right = re.compile('\.[A-Z]{2}')
+
+matrix = get_matrix_from_lines(lines)
+I, J = len(matrix), len(matrix[0])
+
+for i, __l in enumerate(matrix):
+    l = ''.join(__l)
+
+    for m in left.finditer(l):
+        add_portal(portals, m.group()[0:2], i, m.start() + 2, I, J)
+
+    for m in right.finditer(l):
+        add_portal(portals, m.group()[1:], i, m.start(), I, J)
+
 matrix = get_matrix_from_lines(lines)
 matrix = [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]
 
@@ -53,10 +62,10 @@ for i, __l in enumerate(matrix):
     l = ''.join(__l)
 
     for m in left.finditer(l):
-        add_portal(portals, m.group()[0:2], m.start() + 2, i)
+        add_portal(portals, m.group()[0:2], m.start() + 2, i, I, J)
 
     for m in right.finditer(l):
-        add_portal(portals, m.group()[1:], m.start(), i)
+        add_portal(portals, m.group()[1:], m.start(), i, I, J)
 
 coord_to_portal = dict()
 for k, v in portals.items():
@@ -105,16 +114,34 @@ start_node = 'AA-1'
 end_node = 'ZZ-1'
 
 res = u.do_dijkstra(nodes, edges, start_node, end_node)
-
 print(res)
 
 
+# PART 2
+DEPTH = 50
 
+nodes = list()
+edges = defaultdict(lambda: [])
 
+for name in portals.keys():
+    for dep in range(DEPTH):
+        nodes.append(f"{name}--{dep}")
 
+for node in portals.keys():
+    for next_node, d in next_portals(portals, matrix, node):
+        for dep in range(DEPTH):
+            edges[f"{node}--{dep}"].append((f"{next_node}--{dep}", d))
 
+for name in portal_names:
+    if name not in ['AA', 'ZZ']:
+        for dep in range(DEPTH):
+            edges[f"{name}-2--{dep}"].append((f"{name}-1--{dep+1}", 1))
+            edges[f"{name}-1--{dep+1}"].append((f"{name}-2--{dep}", 1))
 
-
+start_node = 'AA-1--0'
+end_node = 'ZZ-1--0'
+res = u.do_dijkstra(nodes, edges, start_node, end_node)
+print(res)
 
 
 
