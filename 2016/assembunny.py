a5233f20f8d473ebb14c01102246bdb48651c116
Louis Chatriot <louischatriot@MacBook-Air-C02KJ1VDQ6L4.local>
2024-11-30 23:01:35 +0100

    2016 day 23 part 1

--- 2016/assembunny.py
+++ 2016/assembunny.py
@@ -2,7 +2,7 @@ registers = ['a', 'b', 'c', 'd']
 
 class Assembunny:
     def __init__(self, program):
-        self.program = program
+        self.program = [l for l in program]
         self.memory = { reg: 0 for reg in registers }
         self.inst_pointer = 0
 
@@ -20,6 +20,8 @@ class Assembunny:
             inst = self.program[self.inst_pointer]
             inst, operands = inst[0:3], inst[4:]
 
+            # print(self.inst_pointer, self.program, self.memory)
+
             if inst == 'inc':
                 self.memory[operands] += 1
                 self.inst_pointer += 1
@@ -30,16 +32,44 @@ class Assembunny:
 
             elif inst == 'cpy':
                 x, y = operands.split()
-                self.memory[y] = self.get_value(x)
+
+                if y in registers:  # Can be made illegal by tgl, in that case skip
+                    self.memory[y] = self.get_value(x)
+
                 self.inst_pointer += 1
 
             elif inst == 'jnz':
                 x, y = operands.split()
-                y = int(y)
+                y = self.get_value(y)
                 if self.get_value(x) != 0:
                     self.inst_pointer += y
                 else:
                     self.inst_pointer += 1
+
+            elif inst == 'tgl':
+                x = operands
+                offset = self.get_value(x)
+
+                if self.inst_pointer + offset < len(self.program):
+                    taddress = self.inst_pointer + offset
+                    tinst = self.program[taddress]
+                    tinst, toperands = tinst[0:3], tinst[4:]
+
+                    if tinst == 'inc':
+                        self.program[taddress] = f"dec {toperands}"
+                    elif tinst in ['dec', 'tgl']:
+                        self.program[taddress] = f"inc {toperands}"
+                    elif tinst == 'jnz':
+                        self.program[taddress] = f"cpy {toperands}"
+                    elif tinst == 'cpy':
+                        self.program[taddress] = f"jnz {toperands}"
+                    else:
+                        raise ValueError("Unexpected instruction, can't toggle")
+
+                self.inst_pointer += 1
+
+
+
 
             else:
                 raise ValueError("Unknown instruction")
---
+++ 2016/day_23.py
@@ -0,0 +1,39 @@
+import sys
+import re
+import u as u
+from collections import defaultdict, deque
+import math
+import itertools
+import os
+
+is_example = (len(sys.argv) > 1)
+fn = os.getcwd() + '/inputs/' + os.path.basename(__file__).replace('.py', '') + ('.example' if is_example else '') + '.data'
+if is_example:
+    print("===== RUNNING THE EXAMPLE =====")
+with open(fn) as file:
+    lines = [line.rstrip() for line in file]
+
+
+# PART 1
+from assembunny import Assembunny
+
+computer = Assembunny(lines)
+if not is_example:
+    computer.memory['a'] = 7
+
+computer.run()
+print(computer.get_value('a'))
+
+
+# PART 2
+computer = Assembunny(lines)
+if not is_example:
+    computer.memory['a'] = 12
+
+computer.run()
+print(computer.get_value('a'))
+
+
+
+
+
