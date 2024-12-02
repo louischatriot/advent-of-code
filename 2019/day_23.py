73eba31bb2a20fe9b1d244ed6711a04dd5cbf50c
Louis Chatriot <louischatriot@MacBook-Air-C02KJ1VDQ6L4.local>
2024-12-02 20:56:04 +0100

    2019 day 23 part 2

--- 2019/day_23.py
+++ 2019/day_23.py
@@ -68,10 +68,49 @@ for c in range(N):
     computers.append(computer)
 
 packets = [list() for _ in range(N)]
+idles = [False for _ in range(N)]
+nat = None
+chief = computers[0]
+last_reboot = None
 
+while True:
+    for c in range(N):
+        computer = computers[c]
+        opcode = computer.run_until_io()
 
+        if computer.is_io_opcode_input(opcode):
+            if len(packets[c]) == 0:
+                computer.run_until_input(-1)
+                idles[c] = True
 
+            else:
+                idles[c] = False
 
+                X, Y = packets[c].pop(0)
+                computer.run_until_input(X)
+                computer.run_until_input(Y)
 
+        else:
+            idles[c] = False
+
+            addr = computer.run_until_output()
+            X = computer.run_until_output()
+            Y = computer.run_until_output()
+
+            if addr == 255:
+                nat = (X, Y)
+            else:
+                packets[addr].append((X, Y))
+
+    if all(i is True for i in idles) and nat is not None:
+        X, Y = nat
+        chief.run_until_input(X)
+        chief.run_until_input(Y)
+
+        if last_reboot == Y:
+            print(Y)
+            break
+        else:
+            last_reboot = Y
 
 
