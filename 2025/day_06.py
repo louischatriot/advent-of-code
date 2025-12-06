ee59b960f293ad7c47006a92e95b29ea67542af1
Louis Chatriot <louischatriot@MacBook-Air-C02KJ1VDQ6L4.local>
2025-12-06 10:07:11 +0100

    Day 6 done

--- 2025/day_06.py
+++ 2025/day_06.py
@@ -11,7 +11,7 @@ fn = os.getcwd() + '/inputs/' + os.path.basename(__file__).replace('.py', '') + 
 if is_example:
     print("===== RUNNING THE EXAMPLE =====")
 with open(fn) as file:
-    lines = [line.rstrip() for line in file]
+    lines = [line[0:-1] for line in file]
 
 
 # PART 1
@@ -25,6 +25,37 @@ for j in range(len(data[0])):
         res += sum(data[i][j] for i in range(N))
     else:
         res += math.prod(data[i][j] for i in range(N))
+
+print(res)
+
+
+# PART 2
+data = list()
+N = len(lines) - 1
+j = len(lines[0]) - 1
+while j > 0:
+    dl = list()
+    while j >= 0:
+        datum = ''
+        for i in range(N):
+            datum += lines[i][j]
+
+        j -= 1
+
+        try:
+            datum = int(datum)
+            dl.append(datum)
+        except:
+            break
+
+    data.append(dl)
+
+res = 0
+for op, dl in zip(reversed(ops), data):
+    if op == '+':
+        res += sum(dl)
+    else:
+        res += math.prod(dl)
 
 print(res)
 
